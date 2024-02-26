a5c51bdc2152e923ba1313653a059d0908eb065a
sungseanpark <sungseanpark@gmail.com>
2024-02-25 20:54:03 -0800

    Complete part 5.5 and 5.6

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import Togglable from './components/Togglable'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -31,9 +33,9 @@ const App = () => {
 
   
   const [blogs, setBlogs] = useState([])
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+  // const [title, setTitle] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [url, setUrl] = useState('')
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('') 
@@ -48,6 +50,8 @@ const App = () => {
       blogService.setToken(user.token)
     }
   }, [])
+
+  const blogFormRef = useRef()
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -79,14 +83,29 @@ const App = () => {
     setUser(null)
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url,
-    }
+  // const addBlog = (event) => {
+  //   event.preventDefault()
+  //   const blogObject = {
+  //     title: title,
+  //     author: author,
+  //     url: url,
+  //   }
 
+  //   blogService
+  //     .create(blogObject)
+  //     .then(returnedBlog => {
+  //       setBlogs(blogs.concat(returnedBlog))
+  //       setNotificationMessage(
+  //         `A new blog '${returnedBlog.title}' by '${returnedBlog.author}' added`
+  //       )
+  //       setTimeout(() => {
+  //         setNotificationMessage(null)
+  //       }, 5000)
+  //     })
+  // }
+
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility()
     blogService
       .create(blogObject)
       .then(returnedBlog => {
@@ -128,35 +147,35 @@ const App = () => {
     </div>
   )
 
-  const blogForm = () => (
-    <div>
-      <h2>create new</h2>
-    <form onSubmit={addBlog}>
-      <div>
-        title:
-          <input
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author:
-          <input
-          value={author}
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url:
-          <input
-          value={url}
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">create</button>
-    </form>
-    </div>
-  )
+  // const blogForm = () => (
+  //   <div>
+  //     <h2>create new</h2>
+  //   <form onSubmit={addBlog}>
+  //     <div>
+  //       title:
+  //         <input
+  //         value={title}
+  //         onChange={({ target }) => setTitle(target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       author:
+  //         <input
+  //         value={author}
+  //         onChange={({ target }) => setAuthor(target.value)}
+  //       />
+  //     </div>
+  //     <div>
+  //       url:
+  //         <input
+  //         value={url}
+  //         onChange={({ target }) => setUrl(target.value)}
+  //       />
+  //     </div>
+  //     <button type="submit">create</button>
+  //   </form>
+  //   </div>
+  // )
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -182,7 +201,11 @@ const App = () => {
           logout
         </button>
       </ul>
-      {blogForm()}
+      <Togglable buttonLabel='new blog' ref={blogFormRef}>
+        <BlogForm
+          createBlog={addBlog}
+        />
+      </Togglable>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
