d87be508279da1c83027a75d8ebd33a29d3abf69
sungseanpark <sungseanpark@gmail.com>
2024-03-02 17:35:55 -0800

    Complete part 5.7

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -207,7 +207,7 @@ const App = () => {
         />
       </Togglable>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} username={user.name} />
       )}
     </div>
   )
--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog, username }) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonLabel, setButtonLabel] = useState('view')
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    if (visible){
+      setButtonLabel('view')
+    }
+    else{
+      setButtonLabel('hide')
+    }
+    setVisible(!visible)
+  }
+
+  return (
+    <div style = {blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>{buttonLabel}</button>
+      </div>
+      <div style = {showWhenVisible}>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>
+          {username}
+        </div>
+      </div>  
+    </div> 
+)}
 
 export default Blog
