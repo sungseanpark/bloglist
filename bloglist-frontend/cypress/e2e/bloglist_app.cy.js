142cb8d47a43dd8afef09213a46f44e0b51c8537
sungseanpark <sungseanpark@gmail.com>
2024-03-16 20:27:56 -0700

    Complete part 5.18'

--- bloglist-frontend/cypress/e2e/bloglist_app.cy.js
+++ bloglist-frontend/cypress/e2e/bloglist_app.cy.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    // create here a user to backend
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').contains('Wrong username or password')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
